c283f711bf9b8c5e6b9326fb989cbae1c0d6a8be
alec <alec@smartalecwebsites.com.au>
2014-04-07 07:44:07 +1000

    row initialisation

--- js/stacker.js
+++ js/stacker.js
@@ -3,6 +3,8 @@ angular.module('stacker', [])
 
     var numRows = 12;
     var numCellsPerRow = 8;
+    var LEFT = -1
+    var RIGHT = 1
     $scope.speed = 200
 
     $scope.grid = []
@@ -19,20 +21,20 @@ angular.module('stacker', [])
       length: 3,
       direction: 1 //move right
     }
-
-    for (var c = $scope.activeBlock.cell; c < $scope.activeBlock.lenth; c++) {
-      $scope.grid[numRows-1][c].active = true
+    var initialiseRow = function () {
+      for (var c = $scope.activeBlock.cell; c < $scope.activeBlock.length + $scope.activeBlock.cell; c++) {
+        $scope.grid[$scope.activeBlock.row][c].active = true
+      }
     }
-
 
     var row;
     var direction = 1
     $scope.step = function() {
-      $scope.activeBlock.cell += $scope.activeBlock.direction
+
       if ( $scope.activeBlock.direction == 1 && ($scope.activeBlock.cell + $scope.activeBlock.length) >= numCellsPerRow) {
-        direction = -1
+        direction = LEFT
       } else if ( $scope.activeBlock.direction == -1 && $scope.activeBlock.cell  == 0) {
-        direction = 1
+        direction = RIGHT
       }
       $scope.activeBlock.direction = direction
 
@@ -42,10 +44,11 @@ angular.module('stacker', [])
         row[$scope.activeBlock.cell+$scope.activeBlock.length].active = true
       } else {
         row[$scope.activeBlock.cell-1].active = true
-        row[$scope.activeBlock.cell+$scope.activeBlock.length-1].active = false
+        row[$scope.activeBlock.cell + $scope.activeBlock.length - 1].active = false
       }
-
+      $scope.activeBlock.cell += $scope.activeBlock.direction
     }
+
 
     var stopBlock = function() {
       if ($scope.activeBlock.row != numRows - 1 && !$scope.grid[$scope.activeBlock.row+1][$scope.activeBlock.cell].active) {
@@ -53,6 +56,10 @@ angular.module('stacker', [])
         $interval.cancel($scope.gameLoop)
       } else if ($scope.activeBlock.row > 0) {
         $scope.activeBlock.row--;
+        if ($scope.activeBlock.row % Math.floor(numRows/3) == 0 && $scope.activeBlock.length > 1) {
+          $scope.activeBlock.length--;
+        }
+        initialiseRow()
         decreaseSpeed()
       } else {
         alert("YOU WIN!")
@@ -70,12 +77,11 @@ angular.module('stacker', [])
       $scope.speed *= 0.95
       $scope.speed -= 2
     }
-
+    initialiseRow()
     $scope.$watch("speed", function(val) {
       if (!!$scope.gameLoop) {
         $interval.cancel($scope.gameLoop)
       }
-      console.log($scope.speed)
       $scope.gameLoop = $interval($scope.step, parseInt($scope.speed, 10));
     })
 
